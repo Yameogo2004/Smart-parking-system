d9a654fceb6bcf119bca96fa0f8347be2548a17d
YAMEOGO Ariel Barthelemy Wendtoin <arielyameogo2004@gmail.com>
2026-05-22 22:57:45 +0100

    Create app_radius.dart

---
+++ app_mobile/lib/core/constants/app_radius.dart
@@ -0,0 +1,12 @@
+class AppRadius {
+  AppRadius._();
+
+  static const double xs = 6.0;
+  static const double sm = 8.0;
+  static const double md = 12.0;
+  static const double lg = 16.0;
+  static const double xl = 20.0;
+  static const double xxl = 24.0;
+  static const double xxxl = 30.0;
+  static const double pill = 999.0;
+}
